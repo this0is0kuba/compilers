19e21f7b0b1326da07c26218e7a7608cf5f5a37f
reczkok <reczkok@gmail.com>
2023-05-31 19:30:28 +0200

    It now makes a pretty tree :D

--- translator/src/scalaToCpp.java
+++ translator/src/scalaToCpp.java
@@ -10,37 +10,46 @@ import java.util.List;
 
 public class scalaToCpp {
     private static class CustomListener extends scalaToCppBaseListener {
+        private static int indent_level = 0;
         @Override
         public void enterEveryRule(ParserRuleContext ctx) {
+            for(int i = 0; i < indent_level; i++) {
+                System.out.print("  |  ");
+            }
+            // make an indentation based on the amount of children
+            indent_level += 1;
             // get every token that is of type identifier
+            // get the token type of the current rule
+            int tokenType = ctx.getRuleIndex();
+            // get the name of the token
+            String tokenName = scalaToCppParser.ruleNames[tokenType];
+            // get the token content (only current rule)
+            String tokenContent = ctx.getText();
+
+            // print the token
+            System.out.print("Entering   " +  "Token: " + tokenName + " - " + tokenContent + "  ");
+
             List<TerminalNode> identifiers = ctx.getTokens(scalaToCppParser.IDENTIFIER);
             if(identifiers.size() > 0) {
                 for( TerminalNode identifier : identifiers) {
-                    System.out.println("    Found identifier: " + identifier.getText());
+                    System.out.print("Found identifier: " + identifier.getText());
                 }
+            }
+            System.out.println();
+        }
+
+        @Override
+        public void exitEveryRule(ParserRuleContext ctx) {
+            indent_level -= 1;
+            for(int i = 0; i < indent_level; i++) {
+                System.out.print("  |  ");
             }
             // get the token type of the current rule
             int tokenType = ctx.getRuleIndex();
             // get the name of the token
             String tokenName = scalaToCppParser.ruleNames[tokenType];
-            // get the token content (only current rule)
-            String tokenContent = ctx.getText();
-
-            // print the token
-            System.out.println("Entering   " +  "Token: " + tokenName + " - " + tokenContent);
-        }
-
-        @Override
-        public void exitEveryRule(ParserRuleContext ctx) {
-            // get the token type of the current rule
-            int tokenType = ctx.getRuleIndex();
-            // get the name of the token
-            String tokenName = scalaToCppParser.ruleNames[tokenType];
-            // get the token content (only current rule)
-            String tokenContent = ctx.getText();
-
-            // print the token
-            System.out.println("Exiting    " +"Token: " + tokenName + " - " + tokenContent);
+            // signal that we are exiting the token
+            System.out.println("Exiting    " +  "Token: " + tokenName);
         }
     }
 
