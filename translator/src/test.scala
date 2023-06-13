80bcf96585aee652bb90578724b2389c546e8f9c
this0is0kuba <jakub.rakus06@gmail.com>
2023-06-13 15:59:31 +0200

    test for

--- translator/src/test.scala
+++ translator/src/test.scala
@@ -10,6 +10,12 @@ private class Class{
             var b = 20;
         }
 
+        var rank = 0;
+        val ranklist = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        for( rank <- ranklist){
+            println("Author rank is : ");
+        }
 
         return x;
     }
