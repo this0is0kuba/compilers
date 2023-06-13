6bc8999adf2fa080cf4267d61d82ac05dcd983d7
this0is0kuba <jakub.rakus06@gmail.com>
2023-06-13 15:39:38 +0200

    test

--- translator/src/test.scala
+++ translator/src/test.scala
@@ -1,6 +1,16 @@
 private class Class{
     def funkcja(arg : Int) : String = {
         val x = "hehe";
+
+        if(x == "hehe") {
+            funckja(2);
+        }
+        else {
+            var a = 10;
+            var b = 20;
+        }
+
+
         return x;
     }
 }
